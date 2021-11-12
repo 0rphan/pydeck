2125d292d094227d9e6acabade775358a3d0cce6
Tal Cherniavsky <talcher1302@gmail.com>
2021-11-12 17:11:16 +0200

    Arduino: Added arduino code to read from numpad

---
+++ pad_reader.ino
@@ -0,0 +1,27 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'0', '1', '2', '3'},
+  {'4', '5', '6', '7'},
+  {'8', '9', 'A', 'B'},
+  {'C', 'D', 'E', 'F'}
+};
+
+byte rowPins[ROWS] = {2, 3, 4, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 7, 8, 9}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  char key = keypad.getKey();
+    // just print the pressed key
+   if (key){
+    Serial.println(key);
+  }
+}
